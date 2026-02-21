12882b7b3fb316bfc5d480fc7890fe8ef62d964e
Sadie3874 <sadie.cutts@icloud.com>
2026-02-21 13:59:25 -0600

    Added a wave spawner for enemies

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<GameObject> enemyList = new List<GameObject>();
+    public List<SpawnEnemy> spawnerList = new List<SpawnEnemy>();
+
     public GoblinFindScript goblinFind;
     void Start()
     {
@@ -16,11 +19,24 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void FindEnemies()
     {
         goblinFind.getClosestEnemy(enemyList);
     }
+
+    //public WaitUntil RotateSpawner()
+    //{
+    //    for(int i = 0; i < spawnerList.Count; i++)
+    //    {
+    //        Debug.Log($"Timer starting for {i}");
+    //        spawnerList[i].startTimer = true;
+    //        yield return new WaitUntil(() => spawnerList[i].isDone == true);
+    //        Debug.Log($"Timer done for {i}");
+    //        spawnerList[i].isDone = false;
+    //    }
+    //   yield return new WaitUntil();
+    //}
 }
--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -9,29 +9,50 @@ public class SpawnEnemy : MonoBehaviour
     public GameObject enemy;
     public GoblinFindScript goblin;
     public GameManager gameManager;
+
+    public int maxEnemies = 3;
+    public bool startTimer = false;
+    public float timer = 0;
+    public float maxTime;
+    public bool isDone = false;
+
+
     void Start()
     {
-
+        startTimer = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (startTimer)
+        {
+            timer += Time.deltaTime;
+            if(timer >= maxTime)
+            {
+                Debug.Log(timer);
+                startTimer = false;
+                timer = 0;
+                SpawnEnemies();
+                
+            }
+        }
     }
 
     public void SpawnEnemies()
     {
-        float randomX = Random.Range(-10, 10);
-        float randomY = Random.Range(0, 10);
+        for(int i = 0; i < maxEnemies; i++)
+        {
+            float randomX = Random.Range(-5, 5);
+            float randomY = Random.Range(0, 5);
 
-        Vector3 randomSpawnPoint = new Vector3(randomX, randomY, 0);
+            Vector3 randomSpawnPoint = new Vector3(randomX, randomY, 0);
 
-        GameObject instantiateEnemy = Instantiate(enemy);
-        instantiateEnemy.transform.position = randomSpawnPoint;
-        
-        gameManager.enemyList.Add(instantiateEnemy);
-        gameManager.FindEnemies();
+            GameObject instantiateEnemy = Instantiate(enemy);
+            instantiateEnemy.transform.position = randomSpawnPoint;
+
+            gameManager.enemyList.Add(instantiateEnemy);
+            gameManager.FindEnemies();
+        }
     }
-
 }
